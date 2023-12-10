f611b17c1905d1285f83de47dcae2df7e0c0358d
Drahiri <kodzidg@o2.pl>
2023-12-10 12:11:18 +0100

    Add Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,45 @@
+#define GLFW_INCLUDE_VULKAN
 #include <cstdlib>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdexcept>
-#include <vulkan/vulkan.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    // Variables
+    GLFWwindow* window;
+
+    // Functions
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Tutorial", nullptr, nullptr);
+    }
+
     void initVulkan() {}
 
-    void mainLoop() {}
+    void mainLoop() {
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
 
-    void cleanup() {}
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
 };
 
 int main() {
