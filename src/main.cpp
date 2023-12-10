05f936a502c2202a93d15ae015a64bc2e57c02b9
Drahiri <kodzidg@o2.pl>
2023-12-10 12:09:28 +0100

    Add general structure base code

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <vulkan/vulkan.h>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {}
+
+    void mainLoop() {}
+
+    void cleanup() {}
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
