be7c848edd4c6fc07e705899533841b7323f0107
Drahiri <kodzidg@o2.pl>
2023-12-14 14:58:26 +0100

    Add Index Buffer

--- src/main.cpp
+++ src/main.cpp
@@ -94,10 +94,13 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-    {{ 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
-    { { 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }},
-    {{ -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }}
+    {{ -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
+    { { 0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }},
+    {  { 0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f }},
+    { { -0.5f, 0.5f }, { 1.0f, 1.0f, 1.0f }}
 };
+
+const std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0 };
 
 class HelloTriangleApplication {
 public:
@@ -130,6 +133,8 @@ private:
     VkCommandPool commandPool;
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
     std::vector<VkCommandBuffer> commandBuffers;
     std::vector<VkSemaphore> imageAvailableSemaphores;
     std::vector<VkSemaphore> renderFinishedSemaphores;
@@ -165,6 +170,7 @@ private:
         createGraphicsPipeline();
         createCommandPool();
         createVertexBuffer();
+        createIndexBuffer();
         createCommandBuffer();
         createSyncObjects();
     }
@@ -184,6 +190,9 @@ private:
             vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
             vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
         }
+
+        vkFreeMemory(device, indexBufferMemory, nullptr);
+        vkDestroyBuffer(device, indexBuffer, nullptr);
 
         vkFreeMemory(device, vertexBufferMemory, nullptr);
         vkDestroyBuffer(device, vertexBuffer, nullptr);
@@ -1066,6 +1075,34 @@ private:
         vkBindBufferMemory(device, buffer, bufferMemory, 0);
     }
 
+    // Index buffer
+    void createIndexBuffer() {
+        VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+        createBuffer(bufferSize,
+              VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+              VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+              stagingBuffer,
+              stagingBufferMemory);
+
+        void* data;
+        vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, indices.data(), bufferSize);
+        vkUnmapMemory(device, stagingBufferMemory);
+
+        createBuffer(bufferSize,
+              VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+              VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+              indexBuffer,
+              indexBufferMemory);
+        copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+        vkFreeMemory(device, stagingBufferMemory, nullptr);
+        vkDestroyBuffer(device, stagingBuffer, nullptr);
+    }
+
     // Command Buffer
     void createCommandBuffer() {
         commandBuffers.resize(MAX_FRAMES_IN_FLIGHT);
@@ -1129,8 +1166,10 @@ private:
         VkDeviceSize offsets[] = { 0 };
         vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
 
+        vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+
         // Actual drawing
-        vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+        vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
         // End Render Pass
         vkCmdEndRenderPass(commandBuffer);
